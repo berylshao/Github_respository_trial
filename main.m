6e02679977d6769c9bb20d2bae3df66d8e3761b0
berylshao <berylshao@berylshaos-MacBook-Air.local>
2015-10-08 21:02:45 -0700

    a little change

---
+++ main.m
@@ -0,0 +1,27 @@
+//
+//  main.m
+//  sample
+//
+//  Created by beryl.shao on 10/8/15.
+//  Copyright © 2015 helloworld. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+void repeatPrint(void) {
+    printf("Hello World\n");
+    //printf("Hello World\n");
+    //printf("Hello World\n");
+}
+
+int main(int argc, const char * argv[]) {
+    repeatPrint();
+    
+    /*comments
+     
+     
+     jjj
+     */
+    
+    return 0;
+}
